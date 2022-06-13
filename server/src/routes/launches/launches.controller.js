2791783f42db1e1faf001831ede464d6d3490b79
rizamoyi <samwelmoyi@gmail.com>
2022-06-13 06:10:21 +0300

    add DELETE launches endpoint and connect to frontend

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,4 +1,9 @@
-const { addNewLaunch, getAllLaunches } = require('../../models/launches.model');
+const {
+  addNewLaunch,
+  getAllLaunches,
+  existsLaunchWithId,
+  abortLaunchById,
+} = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
   return res.status(200).json(getAllLaunches());
@@ -29,7 +34,22 @@ function httpAddNewlaunch(req, res) {
   addNewLaunch(launch);
   return res.status(201).json(launch);
 }
+
+function httpAbortLaunch(req, res) {
+  const launchId = Number(req.params.id);
+
+  if (!existsLaunchWithId(launchId)) {
+    return res.status(404).json({
+      error: 'Launch not found',
+    });
+  }
+
+  const aborted = abortLaunchById(launchId);
+  return res.status(200).json(aborted);
+}
+
 module.exports = {
   httpGetAllLaunches,
   httpAddNewlaunch,
+  httpAbortLaunch,
 };
--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -3,11 +3,13 @@ const express = require('express');
 const {
   httpGetAllLaunches,
   httpAddNewlaunch,
+  httpAbortLaunch,
 } = require('./launches.controller');
 
 const launchesRouter = express.Router();
 
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/', httpAddNewlaunch);
+launchesRouter.delete('/:id', httpAbortLaunch);
 
 module.exports = launchesRouter;
