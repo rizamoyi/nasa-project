c08790bcaa804690591216e690f2fc97eddf5df3
rizamoyi <samwelmoyi@gmail.com>
2022-06-20 06:41:59 +0300

    Get latest flight number from mongodb

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -3,7 +3,7 @@ const planets = require('./planets.mongo');
 
 const launches = new Map();
 
-let latestFlightNumber = 100;
+const DEFAULT_FLIGHT_NUMBER = 100;
 
 const launch = {
   flightNumber: 100,
@@ -20,6 +20,14 @@ saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
   return launches.has(launchId);
+}
+
+async function getLatestFlightNumber() {
+  const latestLaunch = await launchesDatabase.findOne().sort('-flightNumber');
+  if (!latestLaunch) {
+    return DEFAULT_FLIGHT_NUMBER;
+  }
+  return latestLaunch.flightNumber;
 }
 
 async function getAllLaunches() {
