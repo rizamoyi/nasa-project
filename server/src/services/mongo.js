214a01aa12edbecc53556cee0d215c139e7fdf0f
rizamoyi <samwelmoyi@gmail.com>
2022-06-24 23:31:05 +0300

    Manage secrets with dotenv

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-  'mongodb+srv://nasa-api:TwbMPb3VLOaeX9h9@nasacluster.epcof.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection is ready!');
